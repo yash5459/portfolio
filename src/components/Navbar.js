e6ec39fe9e0d4effc3d75df99248ddfcd68e992e
yash5459 <yashkansara1412@gmail.com>
2024-12-30 13:06:01 +0530

    1. navbar on hover color change 2. navbar margin fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,20 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const links = [
-  { href: '/', label: 'Home', icon: <House className='mr-2' /> },
-  { href: '/profile', label: 'Profile', icon: <User className='mr-2' /> },
-  { href: '/contact', label: 'Contact', icon: <NotepadText className='mr-2' /> },
+  { href: '/', label: 'Home', icon: <House className='mr-6 lg:mx-2' /> },
+  { href: '/profile', label: 'Profile', icon: <User className='mr-6 lg:mx-2' /> },
+  { href: '/contact', label: 'Contact', icon: <NotepadText className='mr-6 lg:mx-2' /> },
 ]
 
 const Navbar = () => {
   const pathname = usePathname()
 
   return (
-    <nav className='lg:h-full lg:w-min w-full transform flex lg:flex-col flex-row justify-center lg:border-r lg:border-r-slate-800 lg:border-t-0 border-t border-t-slate-800'>
+    <nav className='lg:h-full lg:w-min w-full transform flex lg:flex-col flex-row justify-center items-center lg:border-r lg:border-r-slate-800 lg:border-t-0 border-t border-t-slate-800'>
       {
         links.map(({ href, label, icon }) => {
           const isActiveLink = href === pathname || pathname?.startsWith(`${href}/`)
-          return <Link key={href} href={href} className={cn('hover:cursor-pointer my-2 lg:mx-2 text-slate-700', isActiveLink && 'text-slate-400')} title={label}>
+          return <Link key={href} href={href} className={cn('hover:cursor-pointer hover:text-slate-500 my-4 lg:mx-2 text-slate-700', isActiveLink && 'text-slate-400')} title={label}>
             {icon}
           </Link>
         })
